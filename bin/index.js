8ab4b467d7e36abfa0fe4b5cbe67d70fed726268
bangbang6 <577174667@qq.com>
2023-04-01 16:18:52 +0800

    feat: init

---
+++ bin/index.js
@@ -0,0 +1,23 @@
+#!/usr/bin/env node
+const lib = require('mengwan-dev-cli-bin')
+const argv = require('process').argv
+const command = argv[2]
+
+const options = argv.slice(3)
+let [option,param] = options
+// 某个命令的option
+if(option){
+  option = option.replace('--','')
+  if(lib[command]){
+    lib[command]({option,param})
+  }else{
+    console.log('请输入正确的命令')
+  }
+}
+//全局option
+if(command.startsWith('--') || command.startsWith('-')){
+  const globalOption = command.replace(/--|-/g,'')
+  if(globalOption === 'version' || globalOption === 'V'){
+    console.log('1.0.0')
+  }
+}
---
+++ packages/core/lib/core.js
@@ -0,0 +1,10 @@
+'use strict';
+
+module.exports = core;
+
+const a = require('@mengwan-dev-cli/utils')
+function core() {
+    // TODO
+    a()
+}
+core()
